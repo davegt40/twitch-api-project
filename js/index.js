70d941239799c8ca722b6de5bb5e495fbc36778d
Dave <davegt40@gmail.com>
2018-02-02 00:22:19 -0500

    Added function to create alerts upon addition of username

--- js/index.js
+++ js/index.js
@@ -6,9 +6,19 @@ function addUser(username) {
   twitchUsers.push(username);
 }
 
+function createAlert(username) {
+  var alertDiv = document.createElement('div');
+  alertDiv.setAttribute('class', 'alert alert-success');
+  alertDiv.setAttribute('role', 'alert');
+  var alertText = document.createTextNode(username + ' added');
+  alertDiv.append(alertText);
+  document.getElementById('user-input-group').append(alertDiv);
+}
+
 $(document).ready(function() {
   $('#add-user').on('click', function() {
     addUser(document.getElementById('username').value);
+    createAlert(document.getElementById('username').value);
     document.getElementById('username').value = null;
   });
 });
