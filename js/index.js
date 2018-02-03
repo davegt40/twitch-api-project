7ddf31b32899210f18d85183bd47d57a819359cb
Dave <davegt40@gmail.com>
2018-02-03 01:33:05 -0500

    Add function to get twitch channel data

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // Default Twitch users to query
-var twitchUsers = ['cunningmoose', 'summit1g', 'shroud', 'freecodecamp',
+var twitchUsers = ['summit1g', 'shroud', 'freecodecamp',
                     'GeekandSundry', 'FeliciaDay'];
 
 function addUser(username) {
@@ -15,10 +15,27 @@ function createAlert(username) {
   document.getElementById('user-input-group').append(alertDiv);
 }
 
+function getUserData() {
+  var channelData = [];
+  for (var i = 0; i < twitchUsers.length; i++) {
+    var channelURL = 'https://wind-bow.gomix.me/twitch-api/channels/';
+    channelURL = channelURL + twitchUsers[i]+ '?callback=?';
+
+    $.getJSON(channelURL, function(data) {
+      console.log(data);
+      channelData.push(data);
+    });
+  }
+  console.log(channelData);
+
+}
+
 $(document).ready(function() {
   $('#add-user').on('click', function() {
     addUser(document.getElementById('username').value);
     createAlert(document.getElementById('username').value);
     document.getElementById('username').value = null;
   });
+
+  getUserData();
 });
