20015928af66e0b63634f9abb30d898d82084c64
Dave <davegt40@gmail.com>
2018-02-04 21:55:10 -0500

    Add user list creation

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // Default Twitch users to query
 var twitchUsers = ['summit1g', 'shroud', 'freecodecamp',
                     'GeekandSundry', 'FeliciaDay'];
+var channelData = [];
 
 function addUser(username) {
   twitchUsers.push(username);
@@ -16,18 +17,50 @@ function createAlert(username) {
 }
 
 function getUserData() {
-  var channelData = [];
   for (var i = 0; i < twitchUsers.length; i++) {
     var channelURL = 'https://wind-bow.gomix.me/twitch-api/channels/';
     channelURL = channelURL + twitchUsers[i]+ '?callback=?';
 
     $.getJSON(channelURL, function(data) {
-      console.log(data);
-      channelData.push(data);
+      var channel = {
+        "display_name": data.display_name,
+        "game": data.game,
+        "status": data.status,
+        "url": data.url
+      }
+      channelData.push(channel);
     });
   }
   console.log(channelData);
+}
 
+function buildUserList() {
+  // wait for objects to be built
+  setTimeout(function() {
+    for (var i = 0; i < channelData.length; i++) {
+      var tableRow = document.createElement('tr');
+      var usernameCell = document.createElement('td');
+      var usernameText = channelData[i].display_name;
+      usernameCell.append(usernameText);
+      tableRow.append(usernameCell);
+
+      var gameCell = document.createElement('td');
+      gameCell.append(channelData[i].game);
+      tableRow.append(gameCell);
+      var statusCell = document.createElement('td');
+      statusCell.append(channelData[i].status);
+      tableRow.append(statusCell);
+
+      var watchCell = document.createElement('td');
+      var watchLink = document.createElement('a');
+      watchLink.setAttribute('href', channelData[i].url);
+      watchLink.append('Watch');
+      watchCell.append(watchLink);
+      tableRow.append(watchCell);
+      
+      document.getElementById('user-list').append(tableRow);
+    }
+  }, 500);
 }
 
 $(document).ready(function() {
@@ -38,4 +71,5 @@ $(document).ready(function() {
   });
 
   getUserData();
+  buildUserList();
 });
